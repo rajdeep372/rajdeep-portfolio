93b7f2837546a882abb4534ba8cec9e5d055b6e3
rajdeep <chatterjeer477@gmail.com>
2026-09-05 21:20:51 +0530

    Updated cv

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -83,7 +83,7 @@ export default function Hero() {
             </a>
 
             <a
-              href="/RAJDEEP_CHATTERJEE_Resume.pdf"
+              href="file:///C:/Users/User/Downloads/Rajdeep_Chatterjee_Resume%20(1).pdf"
               target="_blank"
               rel="noopener noreferrer"
               download="Rajdeep_Chatterjee_Resume.pdf"
